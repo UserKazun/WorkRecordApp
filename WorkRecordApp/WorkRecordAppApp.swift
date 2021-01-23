b584772f07da431798d0f0dfa72c55ca38ae414f
UserKazun <kazuusjlo8@gmail.com>
2021-01-23 19:10:23 +0900

    Initial Commit

---
+++ WorkRecordApp/WorkRecordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkRecordAppApp.swift
+//  WorkRecordApp
+//
+//  Created by kazunari.ueeda on 2021/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkRecordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
