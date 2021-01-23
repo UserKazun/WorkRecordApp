601d89486f6b10e64d6bab1019c7b9ebb1f490c8
UserKazun <kazuusjlo8@gmail.com>
2021-01-23 19:42:35 +0900

    add Tab実装

--- WorkRecordApp/TimerModule/TimerView.swift
+++ WorkRecordApp/TimerModule/TimerView.swift
@@ -1,5 +1,5 @@
 //
-//  HomeView.swift
+//  TimerView.swift
 //  WorkRecordApp
 //
 //  Created by kazunari.ueeda on 2021/01/23.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct HomeView: View {
+struct TimerView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
+struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        TimerView()
     }
 }
