646922b73d9ee47628b06b00828af4eca2c077a7
UserKazun <kazuusjlo8@gmail.com>
2021-01-26 21:09:15 +0900

    タイマー表示含め上部のUI実装

--- WorkRecordApp/TimerModule/TimerView.swift
+++ WorkRecordApp/TimerModule/TimerView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct TimerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+            }
+            .padding()
+            .overlay(
+                HStack {
+                    Image("cloud")
+                        .shadow(color: Color.black.opacity(0.09), radius: 5, x: 2, y: 5)
+                        .offset(x: -25, y: -50)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Image("cloud")
+                            .shadow(color: Color.black.opacity(0.09), radius: 5, x: 2, y: 5)
+                            .offset(x: 20, y: -70)
+                        
+                        Spacer()
+                    }
+                }, alignment: .bottom
+            )
+            
+            VStack {
+                HStack {
+                    Text("ここにタイマー")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .padding(.leading, -75)
+                }
+                
+                Divider()
+            }
+            .padding(25)
+        }
     }
 }
 
